09b002f5c1f38c6e619f91612e12e281aa7aced2
Macan Mehri <macanmehri@gmail.com>
2024-03-06 17:53:27 +0330

    Created a model for human and another for address

---
+++ main.py
@@ -0,0 +1,74 @@
+import peewee
+from database_manager import DatabaseManager
+from local_settings import DATABASE
+
+
+database_manager = DatabaseManager(
+    database_name=DATABASE['name'],
+    user=DATABASE['user'],
+    password=DATABASE['password'],
+    host=DATABASE['host'],
+    port=DATABASE['port']
+)
+
+
+class Address(peewee.Model):
+    '''Address object'''
+
+    city = peewee.CharField(
+        max_length=20,
+        null=False,
+        verbose_name='City'
+    )
+    country = peewee.CharField(
+        max_length=20,
+        null=False,
+        verbose_name='Country'
+    )
+    state = peewee.CharField(
+        max_length=20,
+        null=False,
+        verbose_name='State'
+    )
+    street = peewee.CharField(
+        max_length=255,
+        null=False,
+        verbose_name='Street'
+    )
+    house_name = peewee.CharField(
+        max_length=255,
+        null=False,
+        verbose_name='House Name'
+    )
+
+
+    class Meta:
+        database = database_manager.db
+
+
+
+class Human(peewee.Model):
+    '''A human object'''
+    first_name = peewee.CharField(
+        max_length=50,
+        null=False,
+        verbose_name='First Name'
+    )
+    last_name = peewee.CharField(
+        max_length=50,
+        null=False,
+        verbose_name='Last Name'
+    )
+    number = peewee.CharField(
+        max_length=20,
+        null=False,
+        verbose_name='Number'
+    )
+    address = peewee.ForeignKeyField(
+        model=Address,
+        null=False,
+        verbos_name='Address'
+    )
+
+    class Meta:
+        database = database_manager.db
